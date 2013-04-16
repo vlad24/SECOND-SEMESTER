b4c75a0248d2c9b6ab5a1b4ce001a4170c181ba4
Vladislav Pavlov <vlad.pavlov24@gmail.com>
2013-04-16 22:47:26 +0400

    crosses zeroes second commit(home corrections)

--- TestOneRewritting/CrossesZeroes/GameTable.cpp
+++ TestOneRewritting/CrossesZeroes/GameTable.cpp
@@ -1,7 +1,7 @@
 #include "GameTable.h"
 #include "ui_gametable.h"
-#include "QSignalMapper"
-
+#include <QDebug>
+#include <QSignalMapper>
 int pushings = 0;
 
 GameTable::GameTable(QWidget *parent) :
@@ -9,46 +9,63 @@ GameTable::GameTable(QWidget *parent) :
     ui(new Ui::GameTable)
 {
     ui->setupUi(this);
-    QSignalMapper* buttonMapper = new QSignalMapper();
 
-    connect(ui->button1, SIGNAL(clicked()), buttonMapper, SLOT(map()));
-    buttonMapper->setMapping(ui->button1, ui->button1);
+    for (int i = 0; i <= 2 ; i++)
+        horizontalLines[i] = 0;
+    for (int i = 0; i <= 2 ; i++)
+        verticalLines[i] = 0;
+    for (int i = 0; i <=1; i++)
+        diagonals[i] = 0;
+
+    QSignalMapper* mapper = new QSignalMapper;
+
+    connect(ui->button1, SIGNAL(clicked(bool)), this, SLOT(changeTextOfButton()));
     connect(ui->button1, SIGNAL(clicked(bool)), ui->button1, SLOT(setDisabled(bool)));
+    connect(ui->button1, SIGNAL(clicked(bool)), mapper, SLOT(map()));
+    mapper->setMapping(ui->button1, 1);
 
-    connect(ui->button2, SIGNAL(clicked()), buttonMapper, SLOT(map()));
-    buttonMapper->setMapping(ui->button2, ui->button2);
+    connect(ui->button2, SIGNAL(clicked(bool)), this, SLOT(changeTextOfButton()));
     connect(ui->button2, SIGNAL(clicked(bool)), ui->button2, SLOT(setDisabled(bool)));
+    connect(ui->button2, SIGNAL(clicked(bool)), mapper, SLOT(map()));
+    mapper->setMapping(ui->button2, 2);
 
-    connect(ui->button3, SIGNAL(clicked()), buttonMapper, SLOT(map()));
-    buttonMapper->setMapping(ui->button3, ui->button3);
+    connect(ui->button3, SIGNAL(clicked(bool)), this, SLOT(changeTextOfButton()));
     connect(ui->button3, SIGNAL(clicked(bool)), ui->button3, SLOT(setDisabled(bool)));
+    connect(ui->button3, SIGNAL(clicked(bool)), mapper, SLOT(map()));
+    mapper->setMapping(ui->button3, 3);
 
-    connect(ui->button4, SIGNAL(clicked()), buttonMapper, SLOT(map()));
-    buttonMapper->setMapping(ui->button4, ui->button4);
+    connect(ui->button4, SIGNAL(clicked(bool)), this, SLOT(changeTextOfButton()));
     connect(ui->button4, SIGNAL(clicked(bool)), ui->button4, SLOT(setDisabled(bool)));
+    connect(ui->button4, SIGNAL(clicked(bool)), mapper, SLOT(map()));
+    mapper->setMapping(ui->button4, 4);
 
-    connect(ui->button5, SIGNAL(clicked()), buttonMapper, SLOT(map()));
-    buttonMapper->setMapping(ui->button5, ui->button5);
+    connect(ui->button5, SIGNAL(clicked(bool)), this, SLOT(changeTextOfButton()));
     connect(ui->button5, SIGNAL(clicked(bool)), ui->button5, SLOT(setDisabled(bool)));
+    connect(ui->button5, SIGNAL(clicked(bool)), mapper, SLOT(map()));
+    mapper->setMapping(ui->button5, 5);
 
-    connect(ui->button6, SIGNAL(clicked()), buttonMapper, SLOT(map()));
-    buttonMapper->setMapping(ui->button6, ui->button6);
+    connect(ui->button6, SIGNAL(clicked(bool)), this, SLOT(changeTextOfButton()));
     connect(ui->button6, SIGNAL(clicked(bool)), ui->button6, SLOT(setDisabled(bool)));
+    connect(ui->button6, SIGNAL(clicked(bool)), mapper, SLOT(map()));
+    mapper->setMapping(ui->button6, 6);
 
-    connect(ui->button7, SIGNAL(clicked()), buttonMapper, SLOT(map()));
-    buttonMapper->setMapping(ui->button7, ui->button7);
+    connect(ui->button7, SIGNAL(clicked(bool)), this, SLOT(changeTextOfButton()));
     connect(ui->button7, SIGNAL(clicked(bool)), ui->button7, SLOT(setDisabled(bool)));
+    connect(ui->button7, SIGNAL(clicked(bool)), mapper, SLOT(map()));
+    mapper->setMapping(ui->button7, 7);
 
-    connect(ui->button8, SIGNAL(clicked()), buttonMapper, SLOT(map()));
-    buttonMapper->setMapping(ui->button8, ui->button8);
+    connect(ui->button8, SIGNAL(clicked(bool)), this, SLOT(changeTextOfButton()));
     connect(ui->button8, SIGNAL(clicked(bool)), ui->button8, SLOT(setDisabled(bool)));
+    connect(ui->button8, SIGNAL(clicked(bool)), mapper, SLOT(map()));
+    mapper->setMapping(ui->button8, 8);
 
-    connect(ui->button9, SIGNAL(clicked()), buttonMapper, SLOT(map()));
+    connect(ui->button9, SIGNAL(clicked(bool)), this, SLOT(changeTextOfButton()));
     connect(ui->button9, SIGNAL(clicked(bool)), ui->button9, SLOT(setDisabled(bool)));
-    buttonMapper->setMapping(ui->button9, ui->button9);
+    connect(ui->button9, SIGNAL(clicked(bool)), mapper, SLOT(map()));
+    mapper->setMapping(ui->button9, 9);
 
-
-    connect(buttonMapper, SIGNAL(mapped(QObject*)), this, SLOT(changeTextOfButton(QPushButton*)));
+    connect(mapper, SIGNAL(mapped(int)), this, SLOT(observeGame(int)));
+    connect(this, SIGNAL(gameOver()), this, SLOT(finishGame()));
 }
 
 GameTable::~GameTable()
@@ -56,11 +73,56 @@ GameTable::~GameTable()
     delete ui;
 }
 
-void GameTable::changeTextOfButton(QPushButton* button)
+void GameTable::changeTextOfButton()
 {
+    QPushButton* button = dynamic_cast<QPushButton*>(sender());
     if (pushings % 2 == 0)
         button->setText("X");
     else
         button->setText("O");
     pushings++;
+}
+
+void GameTable::observeGame(int id)
+{
+    int y = (id - 1) / 3;
+    int x = (id - 1) % 3;
+    int input = pushings % 2;
+    if (input == 0)
+        input--;
+    if (x == y)
+    {
+        diagonals[0] += input;
+        if (diagonals[0] == -3 || diagonals[0] == 3)
+            emit gameOver();
+    }
+    if (x + y == 2)
+    {
+        diagonals[1] += input;
+        if (diagonals[1] == -3 || diagonals[1] == 3)
+            emit gameOver();
+    }
+    horizontalLines[y] += input;
+    verticalLines[x] += input;
+    if (horizontalLines[y] == -3 || horizontalLines[y] == 3)
+        emit gameOver();
+    if (verticalLines[x] == -3 || verticalLines[x] == 3)
+        emit gameOver();
+}
+
+void GameTable::finishGame()
+{
+    if (pushings % 2 == 0)
+        ui->label->setText("O win");
+    else
+        ui->label->setText("X win");
+    ui->button1->setDisabled(true);
+    ui->button2->setDisabled(true);
+    ui->button3->setDisabled(true);
+    ui->button4->setDisabled(true);
+    ui->button5->setDisabled(true);
+    ui->button6->setDisabled(true);
+    ui->button7->setDisabled(true);
+    ui->button8->setDisabled(true);
+    ui->button9->setDisabled(true);
 }
--- TestOneRewritting/CrossesZeroes/GameTable.h
+++ TestOneRewritting/CrossesZeroes/GameTable.h
@@ -10,13 +10,21 @@ class GameTable : public QWidget
 {
     Q_OBJECT    
 public slots:
-    void changeTextOfButton(QPushButton *button);
+    void changeTextOfButton();
+    void observeGame(int id);
+    void finishGame();
+signals:
+    void gameOver();
 public:
     explicit GameTable(QWidget *parent = 0);
     ~GameTable();
     
 private:
     Ui::GameTable *ui;
+    int table[3][3];
+    int diagonals[2];
+    int horizontalLines[3];
+    int verticalLines[3];
 
 };
 
